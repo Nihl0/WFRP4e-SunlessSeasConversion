e54a2cf4c32768c23896cf24908dabe07fc4afa4
Dekedras Wright <135967485+Nihl0@users.noreply.github.com>
2026-05-06 02:46:44 -0400

    Updated Insights
    Removed all others, added soot and "slowed" condition

--- scripts/occult-insights.js
+++ scripts/occult-insights.js
@@ -1,211 +1,73 @@
+Hooks.once("init", () => {
+  const config = {
+    magicLores: {},
+    magicWind: {},
+    loreEffectDescriptions: {},
+    loreEffects: {},
+  };
 
-Hooks.on("init", () => {
+  config.magicLores = {
+    bleed: "Bleed",
+    call: "Call",
+    cant: "Cant",
+    depth: "Depth",
+    rose: "Rose",
+    soot: "Soot",
+    silt: "Silt",
+    winter: "Winter",
+    wood: "Wood"
+  }
 
-    const WFRP4E = {}
+  config.magicWind = {
+    bleed: "Bleed",
+    call: "Call",
+    cant: "Cant",
+    depth: "Depth",
+    rose: "Rose",
+    soot: "Soot",
+    silt: "Silt",
+    winter: "Winter",
+    wood: "Wood"
+  }
+  
+  config.loreEffectDescriptions = {
+    bleed: "<p></p>",
+    call: "<p></p>",
+    cant: "<p></p>",
+    depth: "<p></p>",
+    rose: "<p></p>",
+    soot: "<p>Insights of Soot inflicting Damage cause the target to become <em>Slowed</em> for one minute, reducing their Agility by 10 and their Movement by 1 (down to minimum of 3). Target can suffer from only one <em>Slowed</em> Effect at a time.</p>",
+    silt: "<p></p>",
+    winter: "<p></p>",
+    wood: "<p></p>"
+  }
 
-        WFRP4E.loreEffectDescriptions = {
-            "bleed": " <p>Whenever a witch successfully invokes an insight from the Lore of Bleed, the concepts they subsume may restore their own health. Roll [[/r 1d10]]. If the result is a 10, or if it is equal to or higher than the CN of the insight invoked, unmodified by Channeling or the use of a Grimoire, the Witch recovers Wounds equal to the unmodified CN of the spell.</p>",
-            "call": "",
-            "cant": "",
-            "depth": "",
-            "rose": "",
-            "soot": "<p>Soot Spells inflicting Damage cause the target to become <em>Chilled</em> for one minute, reducing their Agility by 10 and their Movement by 1 (down to minimum of 3). Target can suffer from only one <em>Chilled</em> Effect at a time.</p>",
-            "silt": "",
-            "winter": "",
-            "wood": "",
-
-            "weaving": "",
-        };  
-
-
-        // EDGE KNOWLEDGE
-        WFRP4E.loreEffects = {
-            "Bleed": {
-                name: "Lore of Bleed",
-                img: "",
-                flags: {
-                    wfrp4e: {
-                        lore: true,
-                    }
-                },
-
-                system: {
-                    transferData: {
-                        type: "target"
-                    },
-                    scriptData: [
-                        {
-                            trigger: "immediate",
-                            label: "@effect.name",
-                            script: `
-                                if (args.test.result.castOutcome == "success" && !args.test.context.loreApplied)
-                                {
-                                    args.test.context.loreApplied = true
-                
-                                    let roll = Math.ceil(CONFIG.Dice.randomUniform() * 10)
-                                    let cn = args.test.spell._source.system.cn.value;
-                                    let msg = "Bleed Roll: " + roll
-                
-                                    if (roll == 10 || roll >= cn)
-                                    {
-                                        msg += " (Heals " + cn + ")"
-                                        args.test.actor.modifyWounds(cn)
-                                    }
-                                    args.test.result.other.push(msg)
-                                }
-                                `
-                        }
-                    ]
-                }
+  config.loreEffects = {
+        "soot": {
+            name: "Slowed",
+            img: "icons/insights/soot-icon.webp",
+            duration: {
+            seconds: 60
             },
-            "Call": {
-                name: "Lore of Call",
-                img: "",
-                flags: {
-                    wfrp4e: {
-                        lore: true,
-                    }
+            system: {
+                transferData: {
+                    type: "target"
                 },
-
-                system: {
-                    transferData: {
-                        type: "other"
+                scriptData: [
+                    {
+                    label: "@effect.name",
+                    trigger: "prePrepareData",
+                    script: `
+                        args.actor.system.characteristics.ag.modifier -= 10;
+                        
+                        if (args.actor.system.details.move.value > 3)
+                        args.actor.system.details.move.value -= 1;
+                    `
                     }
-                },
-
-            },
-            "Cant": {
-                name: "Lore of Cant",
-                img: "",
-                flags: {
-                    wfrp4e: {
-                        lore: true,
-                    }
-                },
-
-                system: {
-                    transferData: {
-                        type: "other"
-                    }
-                },
-
-            },
-            "Depth": {
-                name: "Lore of Depth",
-                img: "",
-                flags: {
-                    wfrp4e: {
-                        lore: true,
-                    }
-                },
-
-                system: {
-                    transferData: {
-                        type: "target"
-                    },
-                    scriptData: [
-                        {
-                            trigger: "immediate",
-                            label: "@effect.name",
-                            script: `this.actor.addCondition("fatigued")`,
-                            options: {
-                                    deleteEffect: true
-                            }
-                        }
-                    ]
-                }
-            },
-            "Rose": {
-                name: "Lore of Rose",
-                img: "",
-                flags: {
-                    wfrp4e: {
-                        lore: true,
-                    }
-                },
-
-                system: {
-                    transferData: {
-                        type: "other"
-                    }
-                },
-            },
-            "Soot": {
-                name: "Lore of Soot",
-                img: "",
-                flags: {
-                    wfrp4e: {
-                        lore: true,
-                    }
-                },
-
-                system: {
-                    transferData: {
-                        type: "target"
-                    },
-                    scriptData: [
-                        {
-                            trigger: "prePrepareData",
-                            label: "@effect.name",
-                            script: `
-                                args.actor.system.characteristics.ag.modifier -= 10;
-                                
-                                if (args.actor.system.details.move.value > 3)
-                                args.actor.system.details.move.value -= 1;
-                            `
-                        }
-                    ]
-                }
-            },
-            "Silt": {
-                name: "Lore of Silt",
-                img: "modules/wfrp4e-core/icons/spells/witchcraft.png",
-                flags: {
-                    wfrp4e: {
-                        lore: true,
-                    }
-                },
-
-                system: {
-                    transferData: {
-                        type: "other"
-                    }
-                },
-            },
-            "Winter": {
-                name: "Lore of Winter",
-                img: "modules/wfrp4e-core/icons/spells/witchcraft.png",
-                flags: {
-                    wfrp4e: {
-                        lore: true,
-                    }
-                },
-
-                system: {
-                    transferData: {
-                        type: "other"
-                    }
-                },
-            },
-            "Wood": {
-                name: "Lore of Wood",
-                img: "modules/wfrp4e-core/icons/spells/witchcraft.png",
-                flags: {
-                    wfrp4e: {
-                        lore: true,
-                    }
-                },
-
-                system: {
-                    transferData: {
-                        type: "other"
-                    }
-                },
+                ]
             }
+        },
+  };
 
-        }
-
-
-    foundry.utils.mergeObject(game.wfrp4e.config, WFRP4E)
-})
-
+  foundry.utils.mergeObject(game.wfrp4e.config, config);
+});
