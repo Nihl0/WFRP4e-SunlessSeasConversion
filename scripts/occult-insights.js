a81061ed6f5b0e45c8da1b62bb8ba7034a355602
Dekedras Wright <135967485+Nihl0@users.noreply.github.com>
2026-05-07 02:06:07 -0400

    Update occult-insights.js
    Removed scripture, added other lores

--- scripts/occult-insights.js
+++ scripts/occult-insights.js
@@ -1,55 +1,111 @@
 Hooks.once("init", () => {
-  const config = {
-    magicLores: {},
-    magicWind: {},
-    loreEffectDescriptions: {},
-    loreEffects: {},
-  };
+    const config = {
+        magicLores: {},
+        magicWind: {},
+        loreEffectDescriptions: {},
+        loreEffects: {},
+    };
+
+    config.magicLores = {
+        thirst: "Thirst",
+        spirits: "Spirits",
+        shape: "Shape",
+        fathoms: "Fathoms",
+        hues: "Hues",
+        scars: "Scars",
+        ways: "Ways",
+        snow: "Snow",
+        wood: "The Wood"
+    }
 
     config.magicWind = {
-    bleed: "Bleed",
-    call: "Call",
-    cant: "Cant",
-    depth: "Depth",
-    rose: "Rose",
-    scripture: ["Soot", "Cant"],
-    soot: "Soot",
-    silt: "Silt",
-    winter: "Winter",
-    wood: "Wood"
-  }
+        thirst: "Bleed",
+        spirits: "Call",
+        shape: "Cant",
+        fathoms: "Depth",
+        hues: "Rose",
+        scars: "Soot",
+        ways: "Silt",
+        snow: "Winter",
+        wood: "Wood",
+    }
 
-  config.magicLores = {
-    bleed: "Bleed",
-    call: "Call",
-    cant: "Cant",
-    depth: "Depth",
-    rose: "Rose",
-    scripture: "Scripture",
-    soot: "Soot",
-    silt: "Silt",
-    winter: "Winter",
-    wood: "Wood"
-  }
+    config.loreEffectDescriptions = {
+        thirst: "<p>All insights invoked from the Lore of Thirst inﬂicting Damage ignore all non-magical Armor Points, and will bleed to all other targets within 2 yards, except those with the Arcane Magic (Thirst) Talent, inﬂicting hits with a Damage equal to your Willpower Bonus, handled like a magical missile.</p>",
+        spirits: "<p></p>",
+        shape: "<p></p>",
+        fathoms: "<p>Whenever you successfully invoke an insight from the Lore of Fathoms, you may also gain the Fear (1) Creature Trait for the next 1d10 Rounds.</p>",
+        hues: "<p></p>",
+        scars: "<p>All insights invoked from the Lore of Scars inflicting Damage cause the target to become <em>Stained</em> for one minute, reducing their Agility by 10 and their Movement by 1 (down to minimum of 3). Target can suffer from only one <em>Stained</em> Effect at a time.</p>",
+        ways: "<p></p>",
+        snow: "<p></p>",
+        wood: "<p></p>",
+    }
 
-  
-  config.loreEffectDescriptions = {
-    bleed: "<p></p>",
-    call: "<p></p>",
-    cant: "<p></p>",
-    depth: "<p></p>",
-    rose: "<p></p>",
-    soot: "<p>Insights of Soot inflicting Damage cause the target to become <em>Slowed</em> for one minute, reducing their Agility by 10 and their Movement by 1 (down to minimum of 3). Target can suffer from only one <em>Slowed</em> Effect at a time.</p>",
-    silt: "<p></p>",
-    winter: "<p></p>",
-    wood: "<p></p>",
-    scripture: "<p></p>",
-  }
-
-  config.loreEffects = {
-        "soot": {
-            name: "Slowed",
-            img: "icons/insights/soot-icon.webp",
+    config.loreEffects = {
+        "thirst": {
+            name: "Lore of Thirst",
+            img: "modules/WFRP4e-SunlessSeasConversion/icons/insights/bleed-icon.webp",
+            system: {
+                transferData: {
+                    type: "document",
+                    documentType: "Item"
+                },
+                scriptData: [
+                    {
+                    label: "@effect.name",
+                    trigger: "computeApplyDamageModifiers",
+                    script: `
+                            let nonmagical = args.modifiers.ap.value - args.modifiers.ap.magical
+                            if (args.applyAP && nonmagical) 
+                            {
+                                args.modifiers.ap.ignored += nonmagical
+                                args.modifiers.ap.details.push("<strong>" + this.effect.name + "</strong>: Ignore Non-Magical AP (" + nonmagical + ")");
+                            }
+                            `
+                    }
+                ]
+            }
+        },
+        "fathoms": {
+            name: "Lore of Fathoms",
+            img: "modules/WFRP4e-SunlessSeasConversion/icons/insights/depth-icon.webp",
+            system: {
+                transferData: {
+                    type: "document",
+                    documentType: "Item"
+                },
+                scriptData: [
+                    {
+                    label: "Add Fear",
+                    trigger: "castSpellPrayer",
+                    script: "args.testData.other.push(`<strong>${this.effect.name}</strong>: @Fear[1,${this.actor.prototypeToken.name}]`)\n\tif (!this.actor.has(game.i18n.localize(\"NAME.Fear\")))\n\t{\n\t\tlet item = await fromUuid(\"Compendium.wfrp4e-core.items.Item.pTorrE0l3VybAbtn\");\n\t\tlet data = item.toObject();\n\t\tdata.system.specification.value = 1\n\t\tthis.actor.createEmbeddedDocuments(\"Item\", [data])\n\t\tthis.script.scriptNotification(\"Fear Trait added\");\n\t}"
+                    }
+                ]
+            }
+        },
+        "hues": {
+            name: "Lore of Hues",
+            img: "modules/WFRP4e-SunlessSeasConversion/icons/insights/rose-icon.webp",
+            duration: {
+            seconds: 60
+            },
+            system: {
+                transferData: {
+                    type: "target"
+                },
+                scriptData: [
+                    {
+                    label: "Add Distracting",
+                    trigger: "castSpellPrayer",
+                    script: "args.testData.other.push(`<strong>${this.effect.name}</strong>: @Distracting[1,${this.actor.prototypeToken.name}]`)\n\tif (!this.actor.has(game.i18n.localize(\"NAME.Distracting\")))\n\t{\n\t\tlet item = await fromUuid(\"Compendium.wfrp4e-core.items.Item.MVI0lXcg6vvtooAF\");\n\t\tlet data = item.toObject();\n\t\tdata.system.specification.value = 1\n\t\tthis.actor.createEmbeddedDocuments(\"Item\", [data])\n\t\tthis.script.scriptNotification(\"Distracting Trait added\");\n\t}"
+                    }
+                ]
+            }
+        },
+        "scars": {
+            name: "Stained",
+            img: "modules/WFRP4e-SunlessSeasConversion/icons/insights/soot-icon.webp",
             duration: {
             seconds: 60
             },
@@ -71,7 +127,7 @@ Hooks.once("init", () => {
                 ]
             }
         },
-  };
+    };
 
   foundry.utils.mergeObject(game.wfrp4e.config, config);
 });
